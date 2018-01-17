784d00f69bdaa26d7b8ace3e43118dd52f266841
DM Coulter <dcoult01@seattlecentral.edu>
2018-01-16 20:12:07 -0800

    Update Sorting.java
    Fixes bugs to selection and insertion sort variables

--- sorting/sorting/src/sorting/Sorting.java
+++ sorting/sorting/src/sorting/Sorting.java
@@ -52,38 +52,46 @@ public class Sorting {
         }//end outer for
     }//end shellSort
     
-    public static void selectionSort(int[] randomNumbers){
+    public static void selectionSort(int[] arr){
         
-        int n = randomNumbers.length;
-
-        for (int gap = n/2; gap > 0; gap /= 2){
-            for (int i = gap; i < n; i += 1){
-                
-                int temp = randomNumbers[i];
-                int j;
-                for (j = i; j >= gap && randomNumbers[j - gap] > temp; j -= gap)
-                        randomNumbers[j] = randomNumbers[j - gap];
-
-                randomNumbers[j] = temp;
-            }//end inner for
-        }//end outer for
+        
+        int n = arr.length;
+ 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
     }//end selectionSort
         
-    public static void insertionSort(int[] randomNumbers){
+    public static void insertionSort(int[] arr){
         
-        int n = randomNumbers.length;
-
-        for (int gap = n/2; gap > 0; gap /= 2){
-            for (int i = gap; i < n; i += 1){
-                
-                int temp = randomNumbers[i];
-                int j;
-                for (j = i; j >= gap && randomNumbers[j - gap] > temp; j -= gap)
-                        randomNumbers[j] = randomNumbers[j - gap];
-
-                randomNumbers[j] = temp;
-            }//end inner for
-        }//end outer for
+       int n = arr.length;
+        for (int i=1; i<n; ++i)
+        {
+            int key = arr[i];
+            int j = i-1;
+ 
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j>=0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
     }//end insertionSort
     
     public static void main(String[] args) {
